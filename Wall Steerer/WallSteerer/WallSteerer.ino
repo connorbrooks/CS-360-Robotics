aa2de4870f7ffafaf34ee96c751b891c75fcb952
connor <connormbrooks@gmail.com>
2015-10-09 13:29:22 -0500

    Adds angular calculation that approximates distance to approaching wall and tries to correct trajectory before coming into conflict with wall

--- Wall Steerer/WallSteerer/WallSteerer.ino
+++ Wall Steerer/WallSteerer/WallSteerer.ino
@@ -1,5 +1,6 @@
 //Packages
 #include <Arduino.h>
+#include <Math.h>
 
 //Global Variable Declarations
 	
@@ -18,19 +19,37 @@
 			const int leftMotorPin = 3;//LEFT MOTOR PIN HERE ---> NEEDS TO BE BETWEEN 2-13
 			const int rightMotorPin = 2;//RIGHT MOTOR PIN HERE ---> NEEDS TO BE BETWEEN 2-13
 	//GENERAL
-
+    
 		long frontReading;
 		long leftReading;
 		long rightReading;
+    
+	    long lastFront;
+	    long lastLeft;
+	    long lastRight;
+
+	    long angularDist;
+
+	    //[left, right]
+	    boolean adjustsNeeded[] = {false, false};
 
 void setup() {
 	Serial.begin(9600);
 	loadPins();
+
+	frontReading = getSonicDistance(frontSonicInputPin, frontSonicTriggerPin);
+	leftReading = getSonicDistance(leftSonicInputPin, leftSonicTriggerPin);
+	rightReading = getSonicDistance(rightSonicInputPin, rightSonicTriggerPin);
 }
 
 void loop() {
+	lastFront = frontReading;
 	frontReading = getSonicDistance(frontSonicInputPin, frontSonicTriggerPin);
-  
+  	lastLeft = leftReading;
+  	leftReading = getSonicDistance(leftSonicInputPin, leftSonicTriggerPin);
+  	lastRight = rightReading;
+    rightReading = getSonicDistance(rightSonicInputPin, rightSonicTriggerPin);
+
 	Serial.print("Front: ");
 	Serial.print(frontReading);
 	Serial.print(",\tLeft: ");
@@ -40,13 +59,33 @@ void loop() {
 
 	if(frontReading > 70){
 		//go forward
-		analogWrite(leftMotorPin, 60);
-		analogWrite(rightMotorPin, 60);
+
+    	checkAngularDistance();
+
+    	if(adjustsNeeded[0] = true){
+    		//turn left some
+    		analogWrite(leftMotorPin, 25);
+				analogWrite(rightMotorPin, 60);
+    	} else if (adjustsNeeded[1] = true) {
+    		//turn right some
+			  analogWrite(leftMotorPin, 60);
+			  analogWrite(rightMotorPin, 25);
+		  } else {
+        //go straight
+        analogWrite(leftMotorPin, 60);
+        analogWrite(rightMotorPin, 60);   
+		  }
+
+		delay(50);
+
 	} else {
+	//stop movement and poll sides again
 
     analogWrite(leftMotorPin, 0);
     analogWrite(rightMotorPin, 0);
     
+    delay(50);
+
     leftReading = getSonicDistance(leftSonicInputPin, leftSonicTriggerPin);
   
     rightReading = getSonicDistance(rightSonicInputPin, rightSonicTriggerPin);
@@ -62,12 +101,29 @@ void loop() {
 			//turn left
 			analogWrite(rightMotorPin, 50);
 			analogWrite(leftMotorPin, 0);
-		    delay(200);
+      		delay(200);
 		}
 	}
 
 }
 
+
+void checkAngularDistance(){
+	//use delta of side readings to guess which side robot is moving towards
+	if(leftReading - lastLeft > rightReading - lastRight){
+			adjustsNeeded[0] = false;
+			//use geometric identities to estimate absolute distance to nearest wall on left side;
+			angularDist = (long) ( (double)frontReading * sin(atan(((double)leftReading/(double)frontReading))) );
+			//see if we need to turn towards the right some
+			adjustsNeeded[1] = (angularDist < 70);
+	} else {
+			adjustsNeeded[1] = false;
+			//use geometric identities to estimate absolute distance to nearest wall on right side;
+			angularDist = (long) ( (double)frontReading * sin(atan(((double)rightReading/(double)frontReading))) );
+			//see if we need to turn towards the left some
+			adjustsNeeded[0] = (angularDist < 70);
+	}
+}
 
 
 void loadPins(){
@@ -84,6 +140,7 @@ void loadPins(){
 	pinMode(rightMotorPin, OUTPUT);
 }
 
+
 long getSonicDistance(int echoPin, int triggerPin){
 	long distanceCM = 0;
 	digitalWrite(triggerPin,HIGH);
@@ -97,4 +154,5 @@ long getSonicDistance(int echoPin, int triggerPin){
 	}   
 	return (double)distanceCM; 
 }
+
 
